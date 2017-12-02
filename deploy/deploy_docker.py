a60c7bce3bb580d6e72f0e062c54aaec72afe7e1
Bernd Bohmeier <berndbohmeier@googlemail.com>
2017-12-02 16:12:15 +0100

    fix setup script

--- deploy/deploy_docker.py
+++ deploy/deploy_docker.py
@@ -7,7 +7,7 @@ from tlcontracts.deploy import deploy_test_networks
 def main():
     chain_name = "dockerrpc"
     
-    networks = [("Euro", "EUR"), ("US Dollar", "USD"), ("Testcoin", "T")]
+    networks = [("Euro", "EUR", 2), ("US Dollar", "USD", 2), ("Testcoin", "T", 6)]
     
     network_addresses = deploy_test_networks(chain_name, networks)
     
