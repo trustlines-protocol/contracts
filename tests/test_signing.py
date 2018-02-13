305886a41287eeda0c1c7fb021a138c37df3a379
Bernd Bohmeier <berndbohmeier@googlemail.com>
2018-02-13 17:56:08 +0100

    Fix catch now BadSignature exception

--- tests/test_signing.py
+++ tests/test_signing.py
@@ -14,3 +14,11 @@ def test_eth_validate_fail():
     msg_hash2 = (123).to_bytes(32, byteorder='big')
     vrs = eth_sign(msg_hash1, tester.k0)
     assert not eth_validate(msg_hash2, vrs, to_checksum_address(tester.a0))
+
+
+def test_eth_validate_fail2():
+    msg_hash = bytes(32)
+    v = 27
+    r = 18
+    s = 2748
+    assert not eth_validate(msg_hash, (v, r, s), to_checksum_address(tester.a0))
