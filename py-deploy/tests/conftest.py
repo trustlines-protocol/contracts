3a7c6dd678c917cadd88ee40631a3ba0b0ef6690
Ralf Schmitt <ralf@systemexit.de>
2018-09-17 17:18:29 +0200

    make test_deploy.py work

--- py-deploy/tests/conftest.py
+++ py-deploy/tests/conftest.py
@@ -1,6 +1,11 @@
 import os
 import sys
 import pytest
+from web3 import Web3
+from web3.providers.eth_tester import EthereumTesterProvider
+from eth_utils import to_checksum_address, encode_hex
+import eth_tester
+
 
 # import the tlcontracts module so no pip install is necessary
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -17,3 +22,33 @@ def tester():
     t.k0 = b"\x04HR\xb2\xa6p\xad\xe5@~x\xfb(c\xc5\x1d\xe9\xfc\xb9eB\xa0q\x86\xfe:\xed\xa6\xbb\x8a\x11m"
     t.a0 = b"\x82\xa9x\xb3\xf5\x96*[\tW\xd9\xee\x9e\xefG.\xe5[B\xf1"
     return t
+
+
+@pytest.fixture(scope="session")
+def ethereum_tester_session():
+    """Returns an instance of an Ethereum tester"""
+    tester = eth_tester.EthereumTester(eth_tester.PyEVMBackend())
+    k0 = b'\x04HR\xb2\xa6p\xad\xe5@~x\xfb(c\xc5\x1d\xe9\xfc\xb9eB\xa0q\x86\xfe:\xed\xa6\xbb\x8a\x11m'
+    a0 = tester.add_account(encode_hex(k0))
+    faucet = tester.get_accounts()[0]
+    tester.send_transaction(
+        {"from": faucet,
+         "to": to_checksum_address(a0),
+         "gas": 21000,
+         "value": 10000000})
+    return tester
+
+
+@pytest.fixture
+def ethereum_tester(ethereum_tester_session):
+    tester = ethereum_tester_session
+    snapshot = tester.take_snapshot()
+    yield tester
+    tester.revert_to_snapshot(snapshot)
+
+
+@pytest.fixture()
+def web3(ethereum_tester):
+    web3 = Web3(EthereumTesterProvider(ethereum_tester))
+    web3.eth.defaultAccount = web3.eth.accounts[0]
+    return web3
--- py-deploy/tests/test_deploy.py
+++ py-deploy/tests/test_deploy.py
@@ -1,38 +1,15 @@
-import subprocess
-from time import sleep
-
-import pytest
-from populus import Project
-
-from tlcontracts.deploy import deploy_networks, deploy_network
+from tldeploy.core import deploy_networks, deploy_network
 
 
-@pytest.fixture(autouse=True, scope='session')
-def blockchain():
-    p = subprocess.Popen('testrpc-py')
-    sleep(3)  # give it some time to set up
-    yield
-    p.terminate()
+def test_deploy_networks(web3):
+    networks = [("Fugger", "FUG", 2), ("Hours", "HOU", 2), ("Testcoin", "T", 6)]
+    networks, exchange, unw_eth = deploy_networks(web3, networks)
 
-
-@pytest.fixture(scope='session')
-def project():
-    return Project()
-
-
-def test_deploy_networks(project):
-    chain = project.get_chain('testrpclocal')
-
-    networks = [('Fugger', 'FUG', 2), ('Hours', 'HOU', 2), ('Testcoin', 'T', 6)]
-    with chain:
-        networks, exchange, unw_eth = deploy_networks(chain, networks)
-
-    assert networks[0].call().name() == 'Fugger'
+    assert networks[0].call().name() == "Fugger"
     assert unw_eth.call().decimals() == 18
 
 
-def test_deploy_network(project):
-    with project.get_chain('testrpclocal') as chain:
-        network = deploy_network(chain, 'Testcoin', 'T', 2)
+def test_deploy_network(web3):
+    network = deploy_network(web3, "Testcoin", "T", 2)
 
-    assert network.call().name() == 'Testcoin'
+    assert network.call().name() == "Testcoin"
