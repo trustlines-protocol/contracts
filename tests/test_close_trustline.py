32c68f7d94154ece7bf5fe6fef103954c0911668
Ralf Schmitt <ralf@systemexit.de>
2018-11-12 14:23:09 +0100

    test closing a trustline with interests
    we just parametrize the currency_network_contract_with_trustlines with the
    interest rate.
    
    Currently all of the tests with positive interest fail, since we haven't changed
    the contract code yet.

--- tests/test_close_trustline.py
+++ tests/test_close_trustline.py
@@ -1,29 +1,62 @@
 #! pytest
 
+import time
 import pytest
 from tldeploy.core import deploy_network
+
+
+SECONDS_PER_YEAR = 60 * 60 * 24 * 365
 
 
 @pytest.fixture()
 def currency_network_contract(web3):
     return deploy_network(
-        web3, name="TestCoin", symbol="T", decimals=6, fee_divisor=100
+        web3,
+        name="TestCoin",
+        symbol="T",
+        decimals=6,
+        fee_divisor=100,
+        default_interest_rate=0,
+        custom_interests=True,
+        prevent_mediator_interests=False,
     )
 
 
+@pytest.fixture(params=[0, 100, 2000])  # 0% , 1%, 20%
+def interest_rate(request):
+    return request.param
+
+
 @pytest.fixture
-def currency_network_contract_with_trustlines(currency_network_contract, accounts):
+def currency_network_contract_with_trustlines(
+    ethereum_tester_session, currency_network_contract, accounts, interest_rate
+):
+    current_time = int(time.time())
+    ethereum_tester_session.time_travel(current_time + 10)
+
     for a in accounts:
         for b in accounts:
             if a is b:
                 continue
             currency_network_contract.functions.setAccount(
-                a, b, 1000000, 1000000, 0, 0, 0, 0, 0, 0
+                a,
+                b,
+                1000000,  # creditline given
+                1000000,  # creditline received
+                interest_rate,  # interest rate given
+                interest_rate,  # interest rate received
+                0,  # fees outstanding a
+                0,  # fees outstanding b
+                current_time,
+                0,  # balance
             ).transact()
 
     currency_network_contract.functions.transfer(
         accounts[1], 10000, 102, [accounts[1]]
     ).transact({"from": accounts[0]})
+
+    ethereum_tester_session.time_travel(current_time + SECONDS_PER_YEAR)
+
     return currency_network_contract
 
 
