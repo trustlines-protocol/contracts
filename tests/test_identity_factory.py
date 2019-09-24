04604668b3441702eac519a5d579c69b13f05b6d
cducrest <come.ducrest@gmail.com>
2019-09-24 11:57:05 +0200

    Update proxy to properly delegate call to the init of the identity in constructor The initcode of the proxy now contains the owner and no salt is needed for create2

--- tests/test_identity_factory.py
+++ tests/test_identity_factory.py
@@ -40,8 +40,8 @@ def owner(accounts):
 
 def test_build_create2_address_conform_to_EIP1014():
     """
-	Tests out two examples given in https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1014.md
-	"""
+    Tests out two examples given in https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1014.md
+    """
     assert build_create2_address(
         "0x0000000000000000000000000000000000000000", "0x00"
     ) == HexBytes("0x4D1A2e2bB4F88F0250f26Ffff098B0b30B26BF38")
@@ -75,10 +75,10 @@ def build_initcode(contract_assets):
 
 
 def test_deploy_identity_proxy_at_precomputed_address(
-    identity_factory, identity_implementation, build_initcode
+    identity_factory, identity_implementation, build_initcode, owner
 ):
     """Test that we can deploy the proxy at a pre-computed address"""
-    constructor_args = [identity_implementation.address]
+    constructor_args = [identity_implementation.address, owner]
     identity_proxy_initcode = build_initcode("IdentityProxy", constructor_args)
 
     pre_computed_address = build_create2_address(
@@ -92,11 +92,16 @@ def test_deploy_identity_proxy_at_precomputed_address(
     assert HexBytes(identity_proxy_address) == pre_computed_address
 
 
-def test_proxy_bound_to_proper_implementation(
-    identity_factory, web3, contract_assets, identity_implementation, build_initcode
+def test_proxy_constructor_arguments(
+    identity_factory,
+    web3,
+    contract_assets,
+    identity_implementation,
+    build_initcode,
+    owner,
 ):
-    """Test that the proxy has proper value for IdentityImplementation address"""
-    constructor_args = [identity_implementation.address]
+    """Test that the proxy has proper value for IdentityImplementation and owner address"""
+    constructor_args = [identity_implementation.address, owner]
     identity_proxy_initcode = build_initcode("IdentityProxy", constructor_args)
 
     identity_factory.functions.deployProxy(identity_proxy_initcode).transact()
@@ -109,35 +114,15 @@ def test_proxy_bound_to_proper_implementation(
         abi=contract_assets["IdentityProxy"]["abi"],
         bytecode=contract_assets["IdentityProxy"]["bytecode"],
     )
-
-    identity_implementation_address = (
-        identity_proxy_contract.functions.identityImplementation().call()
-    )
-    assert identity_implementation_address == identity_implementation.address
-
-
-def test_init_identity_via_proxy(
-    web3,
-    identity_factory,
-    build_initcode,
-    identity_implementation,
-    owner,
-    contract_assets,
-):
-    constructor_args = [identity_implementation.address]
-    identity_proxy_initcode = build_initcode("IdentityProxy", constructor_args)
-
-    identity_factory.functions.deployProxy(identity_proxy_initcode).transact()
-
-    deployed_event = identity_factory.events.DeployedProxy.getLogs()[0]
-    identity_proxy_address = deployed_event["args"]["proxyAddress"]
-
     proxied_identity_contract = web3.eth.contract(
         address=identity_proxy_address,
         abi=contract_assets["Identity"]["abi"],
         bytecode=contract_assets["Identity"]["bytecode"],
     )
-    proxied_identity_contract.functions.init(owner).transact()
-    proxy_owner = proxied_identity_contract.functions.owner().call()
 
-    assert proxy_owner == owner
+    identity_implementation_address = (
+        identity_proxy_contract.functions.identityImplementation().call()
+    )
+
+    assert identity_implementation_address == identity_implementation.address
+    assert proxied_identity_contract.functions.owner().call() == owner
