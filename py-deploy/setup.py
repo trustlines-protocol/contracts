d6daa9b645fe1b8e88114b518f8d887c33178d49
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-11-07 17:53:13 +0100

    Update required contracts-bin version

--- py-deploy/setup.py
+++ py-deploy/setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=[
         "web3>=4.7.1",
         "click>=7.0",
-        "trustlines-contracts-bin>=0.9.3,<0.10.0",
+        "trustlines-contracts-bin>=0.10.0,<0.11.0",
         "contract-deploy-tools>=0.2.1",
         "attrs>=18.2",
         "pendulum>=2.0.0",
