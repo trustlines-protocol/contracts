9d83f630203a2e27e822fe27f51b866a6f7b1b31
cducrest <come.ducrest@gmail.com>
2020-02-05 12:36:03 +0100

    Make gasPriceDivisor constant so that proxy can use it

--- contracts/identity/Identity.sol
+++ contracts/identity/Identity.sol
@@ -16,7 +16,7 @@ contract Identity is ProxyStorage {
     mapping(bytes32 => bool) private hashUsed;
     uint public lastNonce = 0;
     // Divides the gas price value to allow for finer range of fee price
-    uint public gasPriceDivisor = 1000000;
+    uint constant public gasPriceDivisor = 1000000;
 
     event TransactionExecution(bytes32 hash, bool status);
     event FeePayment(uint value, address indexed recipient, address indexed currencyNetwork);
