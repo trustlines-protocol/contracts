ba64cc1632f684d771dbb907a8b94cb2e6e87082
Bernd Bohmeier <berndbohmeier@googlemail.com>
2020-02-06 15:25:52 +0100

    Correctly set npm version for alpha, beta and rc

--- py-bin/set_npm_version.sh
+++ py-bin/set_npm_version.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env sh
  set -e
 
- VERSION=$(python3 -c 'from setuptools_scm import get_version; print(get_version(root="..").replace(".dev", "-dev").replace("+", "."))')
+ VERSION=$(python3 ./calc_npm_version.py)
  npm version "$VERSION" --allow-same-version
