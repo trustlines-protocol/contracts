469392edac1455d80fd7af7688bcf6965840411a
cducrest <come.ducrest@gmail.com>
2020-02-06 16:09:53 +0100

    Add function to use a hash in identity e.g. to prevent another tx

--- tests/identity/test_identity.py
+++ tests/identity/test_identity.py
@@ -929,6 +929,44 @@ def test_get_version(each_identity_contract):
     assert each_identity_contract.functions.version().call() == 1
 
 
+def test_revoke_meta_transaction_hash(each_identity, delegate, test_contract):
+    to = test_contract.address
+    argument = 10
+    function_call = test_contract.functions.testFunction(argument)
+
+    meta_transaction = each_identity.filled_and_signed_meta_transaction(
+        MetaTransaction.from_function_call(function_call, to=to, nonce=0)
+    )
+    revoke_function_call = each_identity.contract.functions.useHash(
+        meta_transaction.hash
+    )
+    revoke_meta_transaction = each_identity.filled_and_signed_meta_transaction(
+        MetaTransaction.from_function_call(
+            revoke_function_call, to=each_identity.address
+        )
+    )
+    delegate.send_signed_meta_transaction(revoke_meta_transaction)
+
+    assert not delegate.validate_meta_transaction(meta_transaction)
+
+
+def test_revoke_meta_transaction_nonce(each_identity, delegate, test_contract):
+    to = test_contract.address
+    argument = 10
+    function_call = test_contract.functions.testFunction(argument)
+
+    meta_transaction = each_identity.filled_and_signed_meta_transaction(
+        MetaTransaction.from_function_call(function_call, to=to)
+    )
+
+    revoke_meta_transaction = each_identity.filled_and_signed_meta_transaction(
+        MetaTransaction(to=each_identity.address, nonce=meta_transaction.nonce)
+    )
+    delegate.send_signed_meta_transaction(revoke_meta_transaction)
+
+    assert not delegate.validate_meta_transaction(meta_transaction)
+
+
 def test_execute_owner_transaction(owner, accounts, identity_contract, web3):
     recipient = accounts[1]
     transfer_value = 123
