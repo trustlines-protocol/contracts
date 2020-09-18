4cea1adc90655cef300ed99e9483ea672c49b7fd
cducrest <come.ducrest@gmail.com>
2020-09-18 15:27:54 +0200

    Remove unsused acceptId function in currency network basic

--- contracts/currency-network/CurrencyNetworkBasic.sol
+++ contracts/currency-network/CurrencyNetworkBasic.sol
@@ -1247,15 +1247,4 @@ contract CurrencyNetworkBasic is CurrencyNetworkInterface, MetaData, Authorizabl
             return keccak256(abi.encodePacked(_b, _a));
         }
     }
-
-    function acceptId(
-        address _creditor,
-        address _debtor
-    )
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked(_creditor, _debtor));
-    }
 }
