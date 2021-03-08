a0f087fe24f56893dca8f5dfd1c9f099ce479b80
cducrest <come.ducrest@gmail.com>
2021-03-08 20:43:04 +0100

    Add cli methods to deploy new currency network proxies
    Also add method to unfreeze a network and remove its owner

--- py-deploy/tldeploy/cli.py
+++ py-deploy/tldeploy/cli.py
@@ -13,7 +13,10 @@ from deploy_tools.cli import (
     retrieve_private_key,
     validate_address,
 )
-from deploy_tools.transact import build_transaction_options
+from deploy_tools.transact import (
+    build_transaction_options,
+    increase_transaction_options_nonce,
+)
 from eth_utils import is_checksum_address, to_checksum_address
 
 import pendulum
@@ -30,6 +33,9 @@ from .core import (
     deploy_beacon,
     deploy_and_migrate_networks_from_file,
     NetworkSettings,
+    deploy_currency_network_proxy,
+    unfreeze_owned_network,
+    remove_owner_of_network,
 )
 from tldeploy.migration import migrate_networks, verify_networks_migrations
 
@@ -69,6 +75,23 @@ currency_network_contract_name_option = click.option(
     "(e.g. CurrencyNetworkOwnable, CurrencyNetwork, or  TestCurrencyNetwork)",
     default="CurrencyNetwork",
     hidden=False,
+)
+
+beacon_address_option = click.option(
+    "--beacon",
+    "beacon_address",
+    help="Address of the beacon contract the proxy(ies) will point to",
+    required=True,
+    type=str,
+    callback=validate_address,
+)
+proxy_owner_option = click.option(
+    "--owner",
+    "owner_address",
+    help="Address of the owner of the proxies to be deployed",
+    required=True,
+    type=str,
+    callback=validate_address,
 )
 
 
@@ -580,22 +603,8 @@ def beacon(
     default="",
     type=click.Path(dir_okay=False, writable=True),
 )
-@click.option(
-    "--beacon",
-    "beacon_address",
-    help="Address of the beacon contract the proxies will point to",
-    required=True,
-    type=str,
-    callback=validate_address,
-)
-@click.option(
-    "--owner",
-    "owner_address",
-    help="Address of the owner of the proxies to be deployed",
-    required=True,
-    type=str,
-    callback=validate_address,
-)
+@beacon_address_option
+@proxy_owner_option
 @click.option(
     "--output",
     "output_file_path",
@@ -632,4 +641,205 @@ def deploy_and_migrate(
         private_key=private_key,
         transaction_options=transaction_options,
         output_file_path=output_file_path,
+    )
+
+
+@cli.command(short_help="Deploy a currency network proxy contract.")
+@click.argument("name", type=str)
+@click.argument("symbol", type=str)
+@click.option(
+    "--decimals", help="Number of decimals of the network", default=4, show_default=True
+)
+@click.option(
+    "--fee-rate",
+    help="Imbalance fee rate of the currency network in percent",
+    default=0.1,
+    show_default=True,
+)
+@click.option(
+    "--default-interest-rate",
+    help="Set the default interest rate in percent",
+    default=0.0,
+    show_default=True,
+)
+@click.option(
+    "--custom-interests/--no-custom-interests",
+    help="Allow users to set custom interest rates. Default interest rate must be zero",
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--prevent-mediator-interests",
+    help="Disallow payments that would result in mediators paying interests",
+    is_flag=True,
+    default=False,
+)
+@beacon_address_option
+@proxy_owner_option
+@click.option(
+    "--expiration-time",
+    help=(
+        "Expiration time of the currency network after which it will be frozen (0 means disabled). "
+        "Per default the network does not expire."
+    ),
+    required=False,
+    type=int,
+)
+@click.option(
+    "--expiration-date",
+    help=(
+        "Expiration date of the currency network after which it will be frozen "
+        "(e.g. '2020-09-28', '2020-09-28T13:56'). "
+        "Per default the network does not expire."
+    ),
+    type=str,
+    required=False,
+    metavar="DATE",
+    callback=validate_date,
+)
+@jsonrpc_option
+@gas_option
+@gas_price_option
+@nonce_option
+@keystore_option
+def currency_network_proxy(
+    name: str,
+    symbol: str,
+    decimals: int,
+    jsonrpc: str,
+    fee_rate: float,
+    default_interest_rate: float,
+    custom_interests: bool,
+    prevent_mediator_interests: bool,
+    expiration_time: int,
+    expiration_date: pendulum.DateTime,
+    beacon_address: str,
+    owner_address: str,
+    gas: int,
+    gas_price: int,
+    nonce: int,
+    keystore: str,
+):
+    """Deploy an AdministrativeProxy contract eventually pointing towards a currency network contract
+    with custom network settings and proxy owner.
+    If the currency network contract is of type AdministrativeProxy,
+    one may need to unfreeze it and remove the owner to use it."""
+    if custom_interests and default_interest_rate != 0.0:
+        raise click.BadParameter(
+            "Custom interests can only be set without a"
+            " default interest rate, but was {}%.".format(default_interest_rate)
+        )
+
+    if prevent_mediator_interests and not custom_interests:
+        raise click.BadParameter(
+            "Prevent mediator interests is not necessary if custom interests are disabled."
+        )
+
+    if expiration_date is not None and expiration_time is not None:
+        raise click.BadParameter(
+            "Both --expiration-date and --expiration-times have been specified."
+        )
+
+    if expiration_date is None and expiration_time is None:
+        expiration_time = 0
+
+    if expiration_date is not None:
+        expiration_time = int(expiration_date.timestamp())
+
+    fee_divisor = 1 / fee_rate * 100 if fee_rate != 0 else 0
+    if int(fee_divisor) != fee_divisor:
+        raise click.BadParameter("This fee rate is not usable")
+    fee_divisor = int(fee_divisor)
+
+    default_interest_rate = default_interest_rate * 100
+    if int(default_interest_rate) != default_interest_rate:
+        raise click.BadParameter("This default interest rate is not usable")
+    default_interest_rate = int(default_interest_rate)
+
+    web3 = connect_to_json_rpc(jsonrpc)
+    private_key = retrieve_private_key(keystore)
+    nonce = get_nonce(web3=web3, nonce=nonce, private_key=private_key)
+    transaction_options = build_transaction_options(
+        gas=gas, gas_price=gas_price, nonce=nonce
+    )
+
+    network_settings = NetworkSettings(
+        name=name,
+        symbol=symbol,
+        decimals=decimals,
+        fee_divisor=fee_divisor,
+        default_interest_rate=default_interest_rate,
+        custom_interests=custom_interests,
+        prevent_mediator_interests=prevent_mediator_interests,
+        expiration_time=expiration_time,
+    )
+
+    contract = deploy_currency_network_proxy(
+        web3=web3,
+        network_settings=network_settings,
+        beacon_address=beacon_address,
+        owner_address=owner_address,
+        private_key=private_key,
+        transaction_options=transaction_options,
+    )
+
+    click.echo(
+        "CurrencyNetwork(name={name}, symbol={symbol}, "
+        "decimals={decimals}, fee_divisor={fee_divisor}, "
+        "default_interest_rate={default_interest_rate}, "
+        "custom_interests={custom_interests}, "
+        "prevent_mediator_interests={prevent_mediator_interests}): {address}".format(
+            name=name,
+            symbol=symbol,
+            decimals=decimals,
+            fee_divisor=fee_divisor,
+            default_interest_rate=default_interest_rate,
+            custom_interests=custom_interests,
+            prevent_mediator_interests=prevent_mediator_interests,
+            address=to_checksum_address(contract.address),
+        )
+    )
+
+
+@cli.command(
+    short_help="Unfreeze a CurrencyNetworkOwnable contract and remove its owner."
+)
+@click.option(
+    "--address",
+    "currency_network_address",
+    help="Address of the currency network to unfreeze and remove the owner of",
+    required=True,
+    type=str,
+    callback=validate_address,
+)
+@jsonrpc_option
+@gas_price_option
+@nonce_option
+@keystore_option
+def unfreeze_and_remove_owner(
+    currency_network_address: str,
+    jsonrpc: str,
+    gas_price: int,
+    nonce: int,
+    keystore: str,
+):
+    web3 = connect_to_json_rpc(jsonrpc)
+    private_key = retrieve_private_key(keystore)
+    nonce = get_nonce(web3=web3, nonce=nonce, private_key=private_key)
+    transaction_options = build_transaction_options(
+        gas=None, gas_price=gas_price, nonce=nonce
+    )
+
+    unfreeze_owned_network(
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+        currency_network_address=currency_network_address,
+    )
+    increase_transaction_options_nonce(transaction_options)
+    remove_owner_of_network(
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+        currency_network_address=currency_network_address,
     )
