2d9ca81637ba5c949c90f99c0d28d305c991e228
Daniel Dimitrov <daniel@compojoom.com>
2022-11-03 11:27:39 +0100

    Add CurrencyNetworkOwnable v3

---
+++ contracts/currency-network/version3/CurrencyNetworkOwnableV3.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+import "../version2/CurrencyNetworkOwnableV2.sol";
+
+/**
+ * CurrencyNetwork
+ *
+ * Extends basic currency networks to add debt tracking, debit transfer, and onboarding.
+ *
+ **/
+contract CurrencyNetworkOwnableV3 is CurrencyNetworkOwnableV2 {
+
+    /**
+     * A small trick to make the contracts compatible with GnosisSafe
+     * delegate payments.
+     *
+     * @param _creditor The address towards which msg.sender increases its debt
+     * @param _value The value to increase the debt by
+     **/
+    function transfer(address _creditor, uint256 _value) external {
+        CurrencyNetworkV2(address(this)).increaseDebt(_creditor, _value);
+    }
+}
+
+// SPDX-License-Identifier: MIT
